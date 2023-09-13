c48f8a8212d667baa1b64729e379c7e2d7bdbcbe
djknb666 <kai888888@88.com>
2023-09-13 22:05:53 +0800

    Site updated: 2023-09-13 22:05:53

--- js/fomal.js
+++ js/fomal.js
@@ -3174,7 +3174,7 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
+    --default-bg: url(https://dl2.img.timecdn.cn/2023/09/13/IMG_20230817_112433.jpg);
     --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
     --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
